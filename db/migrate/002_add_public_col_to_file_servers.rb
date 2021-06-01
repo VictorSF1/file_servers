1955f6963bbefb0bb0cad1284d74a6cc470ed362
VictorSF1 <59908622+VictorSF1@users.noreply.github.com>
2021-06-01 12:08:42 +0300

    Update 002_add_public_col_to_file_servers.rb

--- db/migrate/002_add_public_col_to_file_servers.rb
+++ db/migrate/002_add_public_col_to_file_servers.rb
@@ -1,4 +1,4 @@
-class AddPublicColToFileServers < ActiveRecord::Migration
+class AddPublicColToFileServers < ActiveRecord::Migration[4.2]
   def change
     add_column :file_servers, :is_public, :boolean
   end
