63b1e80a0ce650c74393f09b5ddbca32f49894ad
VictorSF1 <59908622+VictorSF1@users.noreply.github.com>
2021-06-01 12:03:34 +0300

    Update 003_add_format_store_col_to_file_servers.rb

--- db/migrate/003_add_format_store_col_to_file_servers.rb
+++ db/migrate/003_add_format_store_col_to_file_servers.rb
@@ -1,4 +1,4 @@
-class AddFormatStoreColToFileServers < ActiveRecord::Migration
+class AddFormatStoreColToFileServers < ActiveRecord::Migration[4.2]
   def change
     add_column :file_servers, :format_store, :text
   end
